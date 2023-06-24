acc086704c6025aec6eccda661c6461da29605ef
Sosuke1019 <kato.052wazime@gmail.com>
2023-06-24 10:47:24 +0900

    Fix app.py

--- lineapp/app.py
+++ lineapp/app.py
@@ -120,11 +120,11 @@ def handle_message(event):
     upload_to_s3(s3_file_path, bucket_name, object_name)
     # HTTPSのリンクを取得
     audio_url =  get_s3_https_link(bucket_name, object_name)
-    # short_url = shorten_url(audio_url)
+    short_url = shorten_url(audio_url)
     # メッセージを返信する
     line_bot_api.reply_message(event.reply_token, reply_message)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=audio_url))
-    send_voice_message(line_bot_api,message_id,audio_url)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=short_url))
+    # send_voice_message(line_bot_api,message_id,audio_url)
     os.remove(f"{message_id}.m4a")
     os.remove(f"line_response_audio/{message_id}.m4a")
 
